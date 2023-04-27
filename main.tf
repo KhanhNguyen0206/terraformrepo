52da02f1f62da7dbd4f3a425c647e65c56a92562
OaiLeQuoc <oai.lequoc@petronas.com>
2023-04-27 22:27:05 +0700

    sc commit

--- main.tf
+++ main.tf
@@ -49,7 +49,7 @@ resource "azurerm_app_service" "lab-asv" {
 }
 
 resource "azurerm_storage_account" "lab-sta" {
-  name                     = "lab-storage-account"
+  name                     = "lab-st-account"
   resource_group_name      = azurerm_resource_group.lab-rg.name
   location                 = azurerm_resource_group.lab-rg.location
   account_tier             = "Standard"
